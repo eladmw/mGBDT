70d1b74ea0eb3fad78d70650f269429ea5ddb7e9
eladmw <64884117+eladmw@users.noreply.github.com>
2021-01-14 20:00:18 -0800

    Update online_xgb.py

--- lib/mgbdt/model/online_xgb.py
+++ lib/mgbdt/model/online_xgb.py
@@ -8,7 +8,33 @@ class OnlineXGB(XGBModel):
     def fit_increment(self, X, y, num_boost_round=1, params=None):
         trainDmatrix = DMatrix(X, label=y, nthread=self.n_jobs, missing=self.missing)
         extra_params = params
-        params = self.get_xgb_params()
+        params = {'objective': 'reg:squarederror',
+ 'base_score': None,
+ 'booster': None,
+ 'colsample_bylevel': None,
+ 'colsample_bynode': None,
+ 'colsample_bytree': None,
+ 'gamma': None,
+ 'gpu_id': None,
+ 'importance_type': 'gain',
+ 'interaction_constraints': None,
+ 'learning_rate': None,
+ 'max_delta_step': None,
+ 'max_depth': None,
+ 'min_child_weight': None,
+ 'missing': nan,
+ 'monotone_constraints': None,
+ 'n_estimators': 100,
+ 'n_jobs': None,
+ 'num_parallel_tree': None,
+ 'random_state': None,
+ 'reg_alpha': None,
+ 'reg_lambda': None,
+ 'scale_pos_weight': None,
+ 'subsample': None,
+ 'tree_method': None,
+ 'validate_parameters': None,
+ 'verbosity': None}
         if extra_params is not None:
             for k, v in extra_params.items():
                 params[k] = v
